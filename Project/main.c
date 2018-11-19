ed921851a923f68a5cc5be11c3efcaaa2b7e8e1b
Dmytrii Vitman <dimawittmann@gmail.com>
2018-11-19 14:13:21 +0200

    Remove unused core

--- Project/main.c
+++ Project/main.c
@@ -1,3 +1,4 @@
+
 /*
     FreeRTOS V9.0.0 - Copyright (C) 2016 Real Time Engineers Ltd.
     All rights reserved
@@ -67,344 +68,50 @@
     1 tab == 4 spaces!
 */
 
-/*
- * main() creates all the demo application tasks, then starts the scheduler.
- * The web documentation provides more details of the standard demo application
- * tasks, which provide no particular functionality but do provide a good
- * example of how to use the FreeRTOS API.
- *
- * In addition to the standard demo tasks, the following tasks and tests are
- * defined and/or created within this file:
- *
- * "Check" task - This only executes every five seconds but has a high priority
- * to ensure it gets processor time.  Its main function is to check that all the
- * standard demo tasks are still operational.  While no errors have been
- * discovered the check task will print out "OK" and the current simulated tick
- * time.  If an error is discovered in the execution of a task then the check
- * task will print out an appropriate error message.
- *
- */
-
-
 /* Standard includes. */
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <signal.h>
 
 #include "FreeRTOS.h"
 #include "task.h"
 
-/* Standard demo includes. */
-#include "AbortDelay.h"
-#include "BlockQ.h"
-#include "blocktim.h"
-#include "countsem.h"
-#include "death.h"
-#include "dynamic.h"
-#include "EventGroupsDemo.h"
-#include "flop.h"
-#include "GenQTest.h"
-#include "integer.h"
-#include "IntSemTest.h"
-#include "PollQ.h"
-#include "QPeek.h"
-#include "QueueOverwrite.h"
-#include "QueueSet.h"
-#include "QueueSetPolling.h"
-#include "recmutex.h"
-#include "semtest.h"
-#include "TaskNotify.h"
-#include "TimerDemo.h"
+#define mainTASK_NUMBER                 ( 5 )
+#define mainTASK_PRIORITY               ( tskIDLE_PRIORITY + 1 )
 
-/* Priorities at which the tasks are created. */
-#define mainCHECK_TASK_PRIORITY			( configMAX_PRIORITIES - 2 )
-#define mainQUEUE_POLL_PRIORITY			( tskIDLE_PRIORITY + 1 )
-#define mainSEM_TEST_PRIORITY			( tskIDLE_PRIORITY + 1 )
-#define mainBLOCK_Q_PRIORITY			( tskIDLE_PRIORITY + 2 )
-#define mainCREATOR_TASK_PRIORITY		( tskIDLE_PRIORITY + 3 )
-#define mainINTEGER_TASK_PRIORITY		( tskIDLE_PRIORITY )
-#define mainGEN_QUEUE_TASK_PRIORITY		( tskIDLE_PRIORITY )
-#define mainFLOP_TASK_PRIORITY			( tskIDLE_PRIORITY )
-#define mainQUEUE_OVERWRITE_PRIORITY	( tskIDLE_PRIORITY )
+static void simpleTask( void *pvParameters );
 
-#define mainTIMER_TEST_PERIOD			( 50 )
+static volatile uint uIndex;
 
-
-/*
- * Prototypes for the standard FreeRTOS callback/hook functions implemented
- * within this file.
- */
-void vApplicationMallocFailedHook( void );
-void vApplicationIdleHook( void );
-void vApplicationTickHook( void );
-
-/* Task function to check demo status. */
-static void prvCheckTask( void *pvParameters );
-
-/* The variable into which error messages are latched. */
-static char *pcStatusMessage = "OK";
-
-static bool bExiting = false;
-
-
-static void vExitSignal(int iSig);
-
-
-/*-----------------------------------------------------------*/
 
 int main ( void )
 {
-        TaskHandle_t xTask;
-	/* Start the check task as described at the top of this file. */
-	xTaskCreate( prvCheckTask, "Check", configMINIMAL_STACK_SIZE, NULL, mainCHECK_TASK_PRIORITY, &xTask );
+    xTaskCreate(simpleTask, "simpleTask", configMINIMAL_STACK_SIZE,
+                    NULL, mainTASK_PRIORITY, NULL);
 
-	/* Create the standard demo tasks. */
-	vStartIntegerMathTasks( mainINTEGER_TASK_PRIORITY );
-	vStartMathTasks( mainFLOP_TASK_PRIORITY );
-	vStartBlockingQueueTasks( mainBLOCK_Q_PRIORITY );
-	vStartPolledQueueTasks( mainQUEUE_POLL_PRIORITY );
-	vStartSemaphoreTasks( mainSEM_TEST_PRIORITY );
-	vStartDynamicPriorityTasks();
-	vCreateBlockTimeTasks();
-	vStartGenericQueueTasks( mainGEN_QUEUE_TASK_PRIORITY );
-	vStartQueuePeekTasks();
-	vStartCountingSemaphoreTasks();
-	vStartRecursiveMutexTasks();
+    vTaskStartScheduler();
 
-	vCreateAbortDelayTasks();
-	vStartEventGroupTasks();
-	vStartInterruptSemaphoreTasks();
-	vStartQueueSetTasks();
-	vStartQueueSetPollingTask();
-	vStartQueueOverwriteTask( mainQUEUE_OVERWRITE_PRIORITY );
-	vStartTaskNotifyTask();
-
-	#if( configUSE_PREEMPTION != 0  )
-	{
-		/* Don't expect these tasks to pass when preemption is not used. */
-		vStartTimerDemoTask( mainTIMER_TEST_PERIOD );
-	}
-	#endif
-
-	/* The suicide tasks must be created last as they need to know how many
-	tasks were running prior to their creation.  This then allows them to
-	ascertain whether or not the correct/expected number of tasks are running at
-	any given time. */
-	vCreateSuicidalTasks( mainCREATOR_TASK_PRIORITY );
-
-	signal(SIGINT, vExitSignal);
-
-	/* Start the scheduler itself. */
-	vTaskStartScheduler();
-
-	vTaskDelete(xTask);
-
-	return 0;
+    return 0;
 }
 
-
-/*-----------------------------------------------------------*/
-static void vExitSignal(int iSig)
+static void simpleTask( void *pvParameters )
 {
-	if (iSig == SIGINT)
-		bExiting = true;
+    (void) pvParameters;
+    printf("%s\n", "Hello World");
+    // acording to documentation task should not return, suspend task til exit
+    vTaskSuspend ( NULL );
 }
-/*-----------------------------------------------------------*/
 
-static void prvCheckTask( void *pvParameters )
-{
-TickType_t xNextWakeTime;
-const TickType_t xCycleFrequency = pdMS_TO_TICKS( 2500UL );
 
-	/* Just to remove compiler warning. */
-	( void ) pvParameters;
-
-	/* Initialise xNextWakeTime - this only needs to be done once. */
-	xNextWakeTime = xTaskGetTickCount();
-
-	for( ;; )
-	{
-		/* Place this task in the blocked state until it is time to run again. */
-		vTaskDelayUntil( &xNextWakeTime, xCycleFrequency );
-
-		if (bExiting)
-			vTaskEndScheduler();
-
-		/* Check the standard demo tasks are running without error. */
-		#if( configUSE_PREEMPTION != 0 )
-		{
-			/* These tasks are only created when preemption is used. */
-			if( xAreTimerDemoTasksStillRunning( xCycleFrequency ) != pdTRUE )
-			{
-				pcStatusMessage = "Error: TimerDemo";
-			}
-		}
-		#endif
-
-		if( xAreIntegerMathsTaskStillRunning() != pdTRUE )
-	    {
-			pcStatusMessage = "Error: IntMath";
-	    }
-		else if( xAreMathsTaskStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: Flop";
-		}
-		else if( xAreBlockingQueuesStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: BlockQueue";
-		}
-	    else if( xArePollingQueuesStillRunning() != pdTRUE )
-	    {
-			pcStatusMessage = "Error: PollQueue";
-	    }
-		else if( xAreSemaphoreTasksStillRunning() != pdTRUE )
-	    {
-		    pcStatusMessage = "Error: SemTest";
-	    }
-		else if( xAreDynamicPriorityTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: Dynamic";
-		}
-		else if( xAreBlockTimeTestTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: Block time";
-		}
-		else if( xAreGenericQueueTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: GenQueue";
-		}
-		else if( xAreQueuePeekTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: QueuePeek";
-		}
-		else if( xAreCountingSemaphoreTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: CountSem";
-		}
-		else if( xAreRecursiveMutexTasksStillRunning() != pdTRUE )
-	    {
-			pcStatusMessage = "Error: RecMutex";
-		}
-		else if( xAreAbortDelayTestTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: Abort delay";
-		}
-		else if( xAreEventGroupTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: EventGroup";
-		}
-		else if( xAreInterruptSemaphoreTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: IntSem";
-		}
-		else if( xAreQueueSetTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: Queue set";
-		}
-		else if( xAreQueueSetPollTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: Queue set polling";
-		}
-		else if( xIsQueueOverwriteTaskStillRunning() != pdPASS )
-		{
-			pcStatusMessage = "Error: Queue overwrite";
-		}
-		else if( xAreTaskNotificationTasksStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error:  Notification";
-		}
-		else if( xIsCreateTaskStillRunning() != pdTRUE )
-		{
-			pcStatusMessage = "Error: Death";
-		}
-		
-		/* This is the only task that uses stdout so its ok to call printf()
-		directly. */
-		printf( ( char * ) "%s - %u\n", pcStatusMessage, ( unsigned int ) xTaskGetTickCount() );
-		fflush( stdout );
-	}
-}
-/*-----------------------------------------------------------*/
-
-void vApplicationMallocFailedHook( void )
-{
-	/* vApplicationMallocFailedHook() will only be called if
-	configUSE_MALLOC_FAILED_HOOK is set to 1 in FreeRTOSConfig.h.  It is a hook
-	function that will get called if a call to pvPortMalloc() fails.
-	pvPortMalloc() is called internally by the kernel whenever a task, queue,
-	timer or semaphore is created.  It is also called by various parts of the
-	demo application.  If heap_1.c or heap_2.c are used, then the size of the
-	heap available to pvPortMalloc() is defined by configTOTAL_HEAP_SIZE in
-	FreeRTOSConfig.h, and the xPortGetFreeHeapSize() API function can be used
-	to query the size of free heap space that remains (although it does not
-	provide information on how the remaining heap might be fragmented). */
-	vAssertCalled( __LINE__, __FILE__ );
-}
-/*-----------------------------------------------------------*/
-
-void vApplicationIdleHook( void )
-{
-	/* vApplicationIdleHook() will only be called if configUSE_IDLE_HOOK is set
-	to 1 in FreeRTOSConfig.h.  It will be called on each iteration of the idle
-	task.  It is essential that code added to this hook function never attempts
-	to block in any way (for example, call xQueueReceive() with a block time
-	specified, or call vTaskDelay()).  If the application makes use of the
-	vTaskDelete() API function (as this demo application does) then it is also
-	important that vApplicationIdleHook() is permitted to return to its calling
-	function, because it is the responsibility of the idle task to clean up
-	memory allocated by the kernel to any task that has since been deleted. */
-
-		/* Call the idle task processing used by the full demo.  The simple
-		blinky demo does not use the idle task hook. */
-		//vFullDemoIdleFunction();
-}
-/*-----------------------------------------------------------*/
-
-void vApplicationTickHook( void )
-{
-	/* This function will be called by each tick interrupt if
-	configUSE_TICK_HOOK is set to 1 in FreeRTOSConfig.h.  User code can be
-	added here, but the tick hook is called from an interrupt context, so
-	code must not attempt to block, and only the interrupt safe FreeRTOS API
-	functions can be used (those that end in FromISR()). */
-
-	/* Call the periodic timer test, which tests the timer API functions that
-	can be called from an ISR. */
-	#if( configUSE_PREEMPTION != 0 )
-	{
-		/* Only created when preemption is used. */
-		vTimerPeriodicISRTests();
-	}
-	#endif
-
-	/* Call the periodic queue overwrite from ISR demo. */
-	vQueueOverwritePeriodicISRDemo();
-
-	/* Write to a queue that is in use as part of the queue set demo to
-	demonstrate using queue sets from an ISR. */
-	vQueueSetAccessQueueSetFromISR();
-	vQueueSetPollingInterruptAccess();
-
-	/* Exercise event groups from interrupts. */
-	vPeriodicEventGroupsProcessing();
-
-	/* Exercise giving mutexes from an interrupt. */
-	vInterruptSemaphorePeriodicTest();
-
-	/* Exercise using task notifications from an interrupt. */
-	xNotifyTaskFromISR();
-}
 /*-----------------------------------------------------------*/
 
 void vAssertCalled( unsigned long ulLine, const char * const pcFileName )
 {
- 	taskENTER_CRITICAL();
-	{
+    taskENTER_CRITICAL();
+    {
         printf("[ASSERT] %s:%lu\n", pcFileName, ulLine);
         fflush(stdout);
-	}
-	taskEXIT_CRITICAL();
-	exit(-1);
+    }
+    taskEXIT_CRITICAL();
+    exit(-1);
 }
 /*-----------------------------------------------------------*/
